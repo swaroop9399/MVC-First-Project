d4aed246971d2570c0368ebba507f81b5451dc72
Swaroop Sanjay Patil <swaroop@apl.com>
2022-08-25 17:19:42 +0530

    My first MVC Project

--- MyProject1/Controllers/HomeController.cs
+++ MyProject1/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyProject1.Models;
+using Newtonsoft.Json;
 using System.Data.SqlClient;
+using System.Numerics;
 
 namespace MyProject1.Controllers
 {
@@ -13,6 +15,13 @@ namespace MyProject1.Controllers
         }
 
         public IActionResult AllData()
+        {
+            
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult Data()
         {
             List<User> users = new List<User>();
             string select = "select * from Users";
@@ -27,41 +36,46 @@ namespace MyProject1.Controllers
             }
             cmd.Dispose();
             connection.Close();
-            return View(users);
+            return new JsonResult(users);
+            
         }
         [HttpPost]
-        public RedirectResult Insert(User u)
+        public IActionResult Insert(string requestData)
         {
-            Console.WriteLine(u);
-            DateTime localDate = DateTime.Now;
-            string Insert = "Insert into Users(name, email, password, date1) values(@name, @email, @password, @date1)";
-            SqlConnection connection = new SqlConnection(conString);
-            connection.Open();
-            SqlCommand cmd = new SqlCommand(Insert, connection);
-            cmd.Parameters.AddWithValue("@name", u.Username);
-            cmd.Parameters.AddWithValue("@email", u.Email);
-            cmd.Parameters.AddWithValue("@password", u.Password);
-            cmd.Parameters.AddWithValue("@date1", localDate);
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            connection.Close();
+            var user = JsonConvert.DeserializeObject<User>(requestData);
+            Console.WriteLine(user.Username);
+            //Console.WriteLine(u);
+            //DateTime localDate = DateTime.Now;
+            //string Insert = "Insert into Users(name, email, password, date1) values(@name, @email, @password, @date1)";
+            //SqlConnection connection = new SqlConnection(conString);
+            //connection.Open();
+            //SqlCommand cmd = new SqlCommand(Insert, connection);
+            //cmd.Parameters.AddWithValue("@name", u.Username);
+            //cmd.Parameters.AddWithValue("@email", u.Email);
+            //cmd.Parameters.AddWithValue("@password", u.Password);
+            //cmd.Parameters.AddWithValue("@date1", localDate);
+            //cmd.ExecuteNonQuery();
+            //cmd.Dispose();
+            //connection.Close();
 
-            return Redirect("/");
+            return new JsonResult("Working");
 
         }
 
-        public RedirectResult DeleteProcess(int id)
+        public string DeleteProcess(string id)
         {
-            Console.WriteLine(id);
+           
+            int a = int.Parse(JsonConvert.DeserializeObject<int>(id).ToString());
+
             string Delete = "delete from Users where uid = @id";
             SqlConnection connection = new SqlConnection(conString);
             connection.Open();
             SqlCommand cmd = new SqlCommand(Delete, connection);
-            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@id", a);
             cmd.ExecuteNonQuery();
             cmd.Dispose();
             connection.Close();
-            return Redirect("/");
+            return ("success");
 
         }
         public IActionResult EditProcess(int id)
--- MyProject1/wwwroot/js/site.js
+++ MyProject1/wwwroot/js/site.js
@@ -1,4 +1,64 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "POST",
+        url: "https://localhost:44379/Home/Insert",
+        data: {
+            requestData: JSON.stringify({ Username: "Swaroop" })
+        },
+        success: function (responseObj) {
+            console.log(responseObj)
+        }
+    });
 
-// Write your JavaScript code.
+    ////////////////////////////////////////////////////////////////////////////////////
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:44379/Home/Data",
+        
+        success: setAllData
+    });
+});
+
+function setAllData(responseObj) {
+    console.log(responseObj)
+    const table = document.getElementById("tbody");
+    var row;
+    responseObj.forEach((e) => {
+        row += `
+                <tr id="${e.u_id}">
+                        <th scope="row">${e.u_id}</th>
+                        <td>${e.username}</td>
+                        <td>${e.email}</td>
+                        <td>${e.password}</td>
+                        <td>${e.registerDate}</td>
+                        <td><button class="btn btn-primary" id="edit" value="${e.u_id}">Edit</button></td>
+                        <td>
+                            <button class="btn btn-danger" id="delete" value="${e.u_id}">Delete</button>
+                        </td>
+                    </tr>
+                `;
+    })
+    table.innerHTML = row;
+    document.querySelectorAll("#delete").forEach((e) => {
+        e.addEventListener('click', deleteProcess)
+    });
+    //document.getElementById("edit").addEventListener('click', editProcess);
+}
+
+    /////////////////////////////////////////////////////////////////////////////////
+function deleteProcess(element) {
+    var uid = element.target.value;
+    var b = document.getElementById(uid);
+    b.remove();
+
+
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:44379/Home/DeleteProcess",
+        data: { id: JSON.stringify(uid) },
+        success: function (responseObj) {
+            console.log(responseObj)
+        }
+    });
+}
+
